a7867a411522cc4d0592c724eb58f82861f26a64
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-05-08 00:35:39 +0100

    update informations

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,9 +1,29 @@
 import React ,{Component} from 'react';
 import jwt_decode from 'jwt-decode';
 import './style.css';
+import axios from "axios";
 
 class Profile extends Component
 {
+    updateInformation(newUser)
+    {
+        return axios
+            .post('http://localhost:5000/users/updateUsers',{
+                first_name:newUser.first_name,
+                last_name:newUser.last_name,
+                email:newUser.email
+            })
+            .then(res =>
+            {
+                window.Swal.fire
+                (
+                    'Good job!',
+                    res.data,
+                    'success'
+                )
+            })
+    }
+
     constructor()
     {
         super();
@@ -12,18 +32,50 @@ class Profile extends Component
             first_name:'',
             last_name:'',
             email:'',
-            password:''
+            password:'',
+            newPassword:'',
+            confirmPassword:''
         }
+        this.onChange = this.onChange.bind(this);
+        this.btUpdate = this.btUpdate.bind(this);
     }
+
+    onChange(e)
+    {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    btUpdate()
+    {
+        const newUser=
+            {
+                first_name: this.state.first_name,
+                last_name:this.state.last_name,
+                email: this.state.email
+            }
+        this.updateInformation(newUser).then(res =>
+        {
+
+        })
+    }
+
 
     showUpdate()
     {
         window.$("#updateInterface").show();
+        window.$("#updatePassword").hide();
+    }
+
+    showUpdatePassword()
+    {
+        window.$("#updatePassword").show();
+        window.$("#updateInterface").hide();
     }
 
     componentDidMount()
     {
         window.$("#updateInterface").hide();
+        window.$("#updatePassword").hide();
         const token=localStorage.usertoken;
         const decode=jwt_decode(token);
         console.log("the token is "+token);
@@ -40,10 +92,39 @@ class Profile extends Component
 
     render()
     {
+        const updatePassword=
+            (
+                <div>
+                    <h5>Update my password</h5>
+                    <table>
+                        <tr>
+                            <td><input type="text" placeholder="Old Password" /> </td>
+                            <td>  </td>
+                            <td><input type="text" placeholder="New Password" /></td>
+                            <td>  </td>
+                            <td><input type="text" placeholder="Confirm Password" /></td>
+                            <td>  </td>
+                            <td>
+                                <button   type="button" className="btn btn-primary btn-sm">Submit</button>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+
+            );
         const userUpdate = (
-            <center>
-                <h1>Update Profile</h1>
-            </center>
+            <div>
+                <h5>Update my infos</h5>
+                <table>
+                    <tr>
+                        <td><input type="text" placeholder="First Name" name="first_name" value={this.state.first_name}  onChange={this.onChange} /> </td>
+                        <td><input type="text" placeholder="Last Name"  name="last_name"  value={this.state.last_name}   onChange={this.onChange} /></td>
+                        <td>
+                            <button   type="button" className="btn btn-primary btn-sm" onClick={this.btUpdate}>Submit</button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
         );
         return(
             <div className="container">
@@ -60,7 +141,7 @@ class Profile extends Component
                                      <button onClick={this.showUpdate}  type="button" className="btn btn-primary btn-lg">Update Info</button>
                                  </td>
                                  <td>
-                                     <button  type="button" className="btn btn-primary btn-lg">Update Password</button>
+                                     <button onClick={this.showUpdatePassword} type="button" className="tn btn-secondary btn-lg">Update Password</button>
                                  </td>
                              </tr>
                              <tr>
@@ -73,9 +154,12 @@ class Profile extends Component
                              </tr>
                         </tbody>
                     </table>
-                    <p>{this.state.password}</p>
+                    <p>{this.state.first_name}</p>
                     <div id="updateInterface">
                         {userUpdate}
+                    </div>
+                    <div id="updatePassword">
+                        {updatePassword}
                     </div>
                 </div>
             </div>
