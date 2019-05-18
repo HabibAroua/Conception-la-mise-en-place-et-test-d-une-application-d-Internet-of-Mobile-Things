19fcf7ca1819d196ac91e6400e78d956931d1476
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-05-18 12:33:55 +0100

    last version

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React , {Component} from 'react'
 import axios from "axios";
+import './Login.css';
 
 class Login extends Component
 {
@@ -128,45 +129,50 @@ class Login extends Component
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md mt-5 mx-auto">
-                        <form noValidate onSubmit={this.onSubmit}>
-                            <h1 className="h3 mb-3 font-weight-normal">
-                                Please sign in
-                            </h1>
-                            <div className="form-group">
-                                <label htmlFor="email">
-                                    Email Address
-                                </label>
-                                <input type="email"
-                                       className="form-control"
-                                       name="email"
-                                       placeholder="Enter Email"
-                                       value={this.state.email}
-                                       required
-                                       onChange={this.onChange}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password">
-                                    Password
-                                </label>
-                                <input type="password"
-                                       className="form-control"
-                                       name="password"
-                                       placeholder="Enter Password"
-                                       value={this.state.password}
-                                       required
-                                       onChange={this.onChange}
-                                />
-                            </div>
-                            <button type="submit"
-                                    className="btn btn-lg btn-primary btn-block"
-                            >
-                                Sign in
-                            </button>
-                        </form>
+            <div id="back_ground">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md mt-8 mx-auto">
+                            <form
+                                className="measure " noValidate onSubmit={this.onSubmit}>
+                                <center>
+                                    <h1 className="h3 mb-3 font-weight-normal">
+                                        Please sign in
+                                    </h1>
+                                </center>
+                                <div className='form pa4 br3 shadow-5'>
+                                    <div className="form-group">
+                                        <label htmlFor="email">
+                                            Email Address
+                                        </label>
+                                        <input type="email"
+                                               className="form-control"
+                                               name="email"
+                                               placeholder="Enter Email"
+                                               value={this.state.email}
+                                               onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="password">
+                                            Password
+                                        </label>
+                                        <input type="password"
+                                            className="form-control"
+                                            name="password"
+                                            placeholder="Enter Password"
+                                            value={this.state.password}
+                                               onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <button type="submit"
+                                            className="btn btn-lg btn-primary btn-block"
+                                    >
+                                        Sign in
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
