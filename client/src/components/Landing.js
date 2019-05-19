6cfbff906b1cd04f1c73e76254dbc929afe3cac7
Habib Aroua <HabibAroua@users.noreply.github.com>
2019-05-19 13:58:58 +0100

    update the interface

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,5 +1,5 @@
 import React , {Component} from 'react'
-
+import './style.css'
 class Landing extends Component
 {
     render()
@@ -8,6 +8,15 @@ class Landing extends Component
             <div className="container">
                 <div className="jumbotrom mt-5">
                     <h1 className="text-center">Welcome</h1>
+                    <div id="slider">
+                        <figure>
+                            <img src={require('../images/iot3.jpg')} />
+                            <img src={require('../images/iot1.jpg')} />
+                            <img src={require('../images/iot5.jpg')} />
+                            <img src={require('../images/iot5.jpg')} />
+                            <img src={require('../images/iot.4.jpg')} />
+                        </figure>
+                    </div>
                 </div>
             </div>
         )
